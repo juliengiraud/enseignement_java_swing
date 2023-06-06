0902216c31acb157e73ac12932ed3bdcdb655046
Julien Giraud <giraud740@gmail.com>
2023-06-06 10:31:08 +0200

    fin feature

--- test-git/MonEnum.java
+++ test-git/MonEnum.java
@@ -1,6 +1,12 @@
 public enum Couleur {
-    ROUGE,
-    BLEU,
-    VERT
+    ROUGE("rouge"),
+    BLEU("bleu"),
+    VERT("vert pomme")
     ;
+
+    private String caption;
+
+    private Couleur(String caption) {
+        this.caption = caption;
+    }
 }
