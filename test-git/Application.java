a4309b479cf35da6a106f84b7caf0c7a83873cd1
Julien Giraud <giraud740@gmail.com>
2023-06-06 10:32:39 +0200

    utilisation couleur avant feature

--- test-git/Application.java
+++ test-git/Application.java
@@ -1,5 +1,9 @@
 public class Application {
     public static void main(String[] args) {
         MonEnum couleur = MonEnum.VERT;
+
+        if (couleur == MonEnum.VERT) {
+            System.out.println(couleur);
+        }
     }
 }
